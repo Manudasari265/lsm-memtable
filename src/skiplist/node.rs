1dbfaed1a7cd2c329d2482df4544cbe9ec8c7e0a
Manoj Dasari <dasarimanoj265@gmail.com>
2026-04-15 15:45:18 +0530

    feat: added a constructor method
    Signed-off-by: Manoj Dasari <dasarimanoj265@gmail.com>

--- src/skiplist/node.rs
+++ src/skiplist/node.rs
@@ -2,6 +2,24 @@ use crate::entry::Entry;
 use std::sync::atomic::AtomicPtr;
 
 pub struct Node {
-    pub entry: Option<Entry>, // actual data stored in the node, Option is for head node 
-    pub tower: Box<[AtomicPtr<Node>]>, // array of pointers to next nodes at each level 
+    pub entry: Option<Entry>, // actual data stored in the node, Option is for head node
+    pub tower: Box<[AtomicPtr<Node>]>, // array of pointers to next nodes at each level
+}
+
+impl Node {
+    pub fn new(entry: Option<Entry>, height: usize) -> *mut Node {
+        // build tower: a vector of 'height' AtomicPtrs initialized to null
+        let mut tower = Vec::with_capacity(height);
+        
+        for _ in 0..height {
+            tower.push(AtomicPtr::new(std::ptr::null_mut()));
+        }
+
+        // convert vec into Box<[AtomicPtr<Node>]>
+        let tower = tower.into_boxed_slice();
+        let node = Node { entry, tower };
+
+        // converts the Box<Node> into a raw pointer, which is what we need for the skip list structure
+        Box::into_raw(Box::new(node))
+    }
 }
